b82e2246e08182ab1021773d977512ed6aba9fca
Kennedy2042 <ogbonnakennedy@gmail.com>
2026-05-22 09:09:00 +0100

    template started

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+///////////////////////////////////////
+//FILES
 
 //Blocking synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", 'utf-8');
-// console.log(textIn)
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File written');
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
 
-//Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1)=>{
-fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3)=>{
-        console.log(data3)
+// //Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-        fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, 'utf-8', err=>{
- console.log("your file has been written😂😂😂😂😂😂😂😂😂😂😘😘👌😘👌👌👌👌")
-        })
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log(
+//           "your file has been written😂😂😂😂😂😂😂😂😂😂😘😘👌😘👌👌👌👌",
+//         );
+//       });
+//     });
+//   });
+// });
+// console.log("will read file");
+
+////////////////////////////////////////////////
+//SERVER
+ const data =fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+      const dataObj = JSON.parse(data);
+
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT page");
+  } else if (pathName === "/api") {
+
+      res.writeHead(200, {
+        "content-type": "application/json",
+      });
+      res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+
+  //   res.end("Hello from the server!");
 });
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server is listening to request on port 8000");
 });
-});
-console.log("will read file")
