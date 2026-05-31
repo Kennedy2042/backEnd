a46a7ab53cdbcda962117ea979d024e586cd51f2
Kennedy2042 <ogbonnakennedy@gmail.com>
2026-05-31 06:07:54 +0100

    getting better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////////////
 //FILES
@@ -31,26 +32,51 @@ const url = require("url");
 
 ////////////////////////////////////////////////
 //SERVER
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8",
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8",
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8",
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const {query, pathname}=(url.parse(req.url, true));
+  
 
   //Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, {
-        "content-type": 'text/html'
-    })
-    res.end(tempOverview);
+      "content-type": "text/html",
+    });
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+
+    res.end(output);
 
     // Product page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT page");
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "content-type": 'text/html'
+    })
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product)
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
